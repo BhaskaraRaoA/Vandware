a52e8f1a3f76a4e9c075a4a82cdce64d3ea09442
bhask <bhask@BHASKAR>
2023-04-12 11:31:40 +0530

    commited done

--- src/test/java/sample/DemoRun.java
+++ src/test/java/sample/DemoRun.java
@@ -11,4 +11,13 @@ public class DemoRun {
 		System.out.println(" GIT and GITHUB Projects");
 	}
 	
+public void bhaskarAnnepu()
+{
+	System.out.println("name: bhaskar");
+	System.out.println("age : 26");
+	System.out.println("Qualification : Master's Science");
+	System.out.println("Skills : Manual Testing, Automation Testing, Core Java, MVN Project, J meter, ALM, JIRA tool, Selenium WebDriver");
+	System.out.println("Campany name : Sustine Solutions Pvt Ltd");
+	System.out.println("Address : Chanda nagar, Lingampally, Hyderabad, Telangana, pincode :- 500050");
+}
 }
