c741e3e691f89aa95d962877541a147c1e6ae9fc
bhask <bhaskar.bh143@gmail.com>
2023-04-13 11:28:47 +0530

    committed by bhaskar

--- src/test/java/sample/DemoRun.java
+++ src/test/java/sample/DemoRun.java
@@ -9,6 +9,9 @@ public class DemoRun {
 		System.out.println("demo the test");
 		System.out.println("Bhaskar test scripts");
 		System.out.println(" GIT and GITHUB Projects");
+		
+		
+		System.out.println(" chances the requirements ");
 	}
 	
 public void bhaskarAnnepu()
@@ -19,5 +22,8 @@ public void bhaskarAnnepu()
 	System.out.println("Skills : Manual Testing, Automation Testing, Core Java, MVN Project, J meter, ALM, JIRA tool, Selenium WebDriver");
 	System.out.println("Campany name : Sustine Solutions Pvt Ltd");
 	System.out.println("Address : Chanda nagar, Lingampally, Hyderabad, Telangana, pincode :- 500050");
+	
+	
+	System.out.println(" ==== hello bhaskar today have a greate day ==== ");
 }
 }
